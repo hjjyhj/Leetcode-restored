df0a3246f17d0030ee83e02a8e1a861e4245fe8c
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2021-12-02 16:49:06 -0500

    tried using unordered_set, not working

--- Blind 75 LeetCode/Longest Substring Without Repeating Characters/longest.cpp
+++ Blind 75 LeetCode/Longest Substring Without Repeating Characters/longest.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 int check(string s, int start, int end) {
-    int leng = 0;
-    
+    int leng = 1;
+    unordered_set<char> set;
+    for (int i = start + 1; i <= end; ++i) {
+        if (set.find(s[start]) != set.end()) {
+            return leng;
+            
+        } else if (set.find(s[start]{
+            ;
+        }
+    }
     return leng;
 }
 
 int lengthOfLongestSubstring(string s) {
     int answer = 0;
-    int current;
+    int current = 0;
     for (int i = 0; i < s.length(); ++i) {
         for (int j = i; j < s.length(); ++j) {
             current = check(s, i, j);
@@ -27,4 +35,7 @@ int main() {
     string s = "abcabcbb";
     int ans = lengthOfLongestSubstring(s);
     cout << ans << endl;
+   
+
+
 }
