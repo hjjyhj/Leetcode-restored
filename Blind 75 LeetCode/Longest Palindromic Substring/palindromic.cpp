37f0197a29358d9bf9ab702fa4c2011cb2c2eb6c
hjjyhj <95295245+hjjyhj@users.noreply.github.com>
2021-12-07 15:44:32 -0500

    Doesn't work for same characters
    Need modification

--- Blind 75 LeetCode/Longest Palindromic Substring/palindromic.cpp
+++ Blind 75 LeetCode/Longest Palindromic Substring/palindromic.cpp
@@ -4,35 +4,41 @@
 using namespace std;
 
 string expand(string s, int start, int end) {
-    while (start > 0 && end < s.length() && s[start] == s[end]) {
-        --start;
-        ++end;
+    string ans = "";
+    while (start > 0 && end < s.length() && s[start] == s[end]){
+        ans = s.substr(start, end-start+1);
+        start--;
+        end++;
     }
-    return s.substr(start+1, end-start-1);
+    return ans;
 }
+
 string longestPalindrome(string s) {
     string longest = s.substr(0,1);
-    if (s.length() <=1) {
+    string ans = s.substr(0,1);
+    if (s.length() == 1) {
         return s;
     }
 
     for (int i = 0; i < s.length(); ++i) {
-        string temp = expand(s, i, i);
-        
-        if (temp.length() > longest.length()) {
-            longest = temp;
-        }
-        temp = expand(s,i,i+1);
-        if (temp.length() > longest.length()) {
-            longest = temp;
+        if (s.length()%2 == 0) {
+            string result = expand(s, i-1, i);
+            if (result.length() > ans.length()) {
+                ans = result;
+            }
+        } else {
+            string result = expand(s, i-1, i+1);
+            if (result.length() > ans.length()) {
+                ans = result;
+            }        
         }
     }
-    return longest;
-
+    return ans;
 }
 
+
 int main() {
-    string s = "babad";
+    string s = "bbbbb";
     string sub;
     cout << longestPalindrome(s) << endl;
 
