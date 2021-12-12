2daf3f70bad541e572e4ff914fa775208e460d5e
hjjyhj <hojungkim123@gmail.com>
2021-12-11 21:04:49 -0500

    3Sum revised
    not working

--- Blind 75 LeetCode/3Sum/3sum.cpp
+++ Blind 75 LeetCode/3Sum/3sum.cpp
@@ -4,22 +4,54 @@
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int> &nums) {
+    unordered_map <int, int> map;
+    vector<int> noDup;
     vector<vector<int>> ans;
-    
+    for (int i = 0; i < nums.size(); ++i) {
+        map[nums[i]] = i;
+    }
+
+    for (auto i = map.begin(); i != map.end(); i++) {
+        noDup.push_back((*i).first);
+    }
+
     int p1 = 0;
     int p2 = 1;
     int p3 = 2;
-    while (p1 < nums.size() - 2) {
-        if (-nums[p1] == nums[p2] + nums[p3] && nums[p1]){
-            vector<int> a;
-            ans.push_back({nums[p1], nums[p2], nums[p3]});
+
+    if (nums.size() < 2) {
+        return ans;
+    }   else if (nums.size() == 3) {
+            if (nums[p1]+nums[p2]+nums[p3] == 0) {
+                ans.push_back({nums[p1], nums[p2], nums[p3]});
+                return ans;
+            }
+    }
+    
+    while (p1 < noDup.size() - 2) {
+        if (noDup.size() < 2) {
+            return ans;
         }
 
-        if (p3 == nums.size() && p2 == nums.size() -1) {
+        if (noDup.size() == 3) {
+            if (noDup[p1] + noDup[p2] + noDup[p3] == 0) {
+                vector<int> a;
+                ans.push_back({noDup[p1], noDup[p2], noDup[p3]});
+                return ans;
+                break;
+            }
+        }
+
+        if (noDup[p1] + noDup[p2] + noDup[p3] == 0) {
+            vector<int> a;
+            ans.push_back({noDup[p1], noDup[p2], noDup[p3]});
+        }
+
+        if (p3 == noDup.size() && p2 == noDup.size() -1) {
             ++p1;
             p2 = p1 + 1;
             p3 = p2 + 1;
-        } else if (p3 == nums.size() && p2 != nums.size() - 1) {
+        } else if (p3 == noDup.size() && p2 != noDup.size() - 1) {
             ++p2;
             p3 = p2 + 1;
         } else {
@@ -42,4 +74,19 @@ int main() {
         }
         cout << endl;
     }
+
+    // unordered_map <int, int> map;
+    // vector<int> noDup;
+    // vector<vector<int>> ans;
+    // for (int i = 0; i < nums.size(); ++i) {
+    //     map[nums[i]] = i;
+    // }
+
+    // for (auto i = map.begin(); i != map.end(); i++) {
+    //     noDup.push_back(i->first);
+    // }
+    
+    // for (int i = 0; i < noDup.size(); ++i) {
+    //     cout << noDup[i] << endl;
+    // }
 }
