656511dddc08fc5d8ff72fb6c94a087f82349db9
hjjyhj <hojungkim123@gmail.com>
2021-12-12 20:23:53 -0500

    3sum revised2
    Solving []

--- Blind 75 LeetCode/3Sum/3sum.cpp
+++ Blind 75 LeetCode/3Sum/3sum.cpp
@@ -4,35 +4,61 @@
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int> &nums) {
-
     vector<vector<int>> ans;
-    sort(nums.begin(), nums.end());
+    
+    int p1 = 0;
+    int p2 = 1;
+    int p3 = 2;
 
+    if (nums.size() == 0) {
+        return {};
+    }
+
+    bool zero = true;
     for (int i = 0; i < nums.size(); ++i) {
-        int start = i + 1;
-        int end = nums.size() - 1;
-        if(i > 0 && nums[i] == nums[i-1]) {
-            continue; 
-        }
-
-        while (start < end) {
-            if (nums[i] + nums[start] + nums[end] == 0) {
-                ans.push_back({nums[i], nums[start], nums[end]});
-                start++;
-                end--;
-            } else if (nums[i] + nums[start] + nums[end] < 0) {
-                start++;
-            } else {
-                end--;
-            }
+        if (nums[i] != 0) {
+            zero = false;
         }
     }
-    return ans;
+    if (zero) {
+        ans.push_back({0,0,0});
+        return ans;
+    }
+
+    if (nums.size() < 2) {
+        return ans;
+    } else if (nums.size() == 3) {
+        if (nums[p1] + nums[p2] + nums[p3] == 0) {
+            vector<int> a;
+            ans.push_back({nums[p1], nums[p2], nums[p3]});
+            return ans;
+        }
+    }
+
+    
+    while (p1 < nums.size() - 2 && nums.size() > 3) {
+        
+        if (-nums[p1] == nums[p2] + nums[p3] && nums[p1]){
+            vector<int> a;
+            ans.push_back({nums[p1], nums[p2], nums[p3]});
+        }
+
+        if (p3 == nums.size() && p2 == nums.size() -1) {
+            ++p1;
+            p2 = p1 + 1;
+            p3 = p2 + 1;
+        } else if (p3 == nums.size() && p2 != nums.size() - 1) {
+            ++p2;
+            p3 = p2 + 1;
+        } else {
+            ++p3;
+        }
+    }
+    return ans;   
 }
 
-
 int main() { 
-    vector<int> nums = {-1,0,1,2,-1,-4};
+    vector<int> nums = {0,0};
     vector<vector<int>> a;
     a =  threeSum(nums);
     for (int i = 0; i < a.size(); ++i) {
@@ -43,18 +69,5 @@ int main() {
         cout << endl;
     }
 
-    // unordered_map <int, int> map;
-    // vector<int> noDup;
-    // vector<vector<int>> ans;
-    // for (int i = 0; i < nums.size(); ++i) {
-    //     map[nums[i]] = i;
-    // }
 
-    // for (auto i = map.begin(); i != map.end(); i++) {
-    //     noDup.push_back(i->first);
-    // }
-    
-    // for (int i = 0; i < noDup.size(); ++i) {
-    //     cout << noDup[i] << endl;
-    // }
 }
