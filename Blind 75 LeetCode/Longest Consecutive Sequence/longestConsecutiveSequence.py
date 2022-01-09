d154cc8235f7612760729de7f2bcba86758b8713
hjjyhj <hojungkim123@gmail.com>
2022-01-09 16:46:14 -0500

    Longest Consecutive Sequence.py

---
+++ Blind 75 LeetCode/Longest Consecutive Sequence/longestConsecutiveSequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nums.sort()
+        i = 1
+        m = 0
+        cur = 1
+        if len(nums) == 1:
+            return 1
+        
+        while i < len(nums):
+            
+            if nums[i-1] + 1 == nums[i]:
+                cur += 1
+            else:
+                cur = 1
+            i += 1
+            m = max(m, cur)
+        return m
+        
+        #1,2,3,4,100,200,300
