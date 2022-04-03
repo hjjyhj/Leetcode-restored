b1a4f8f21b937325c3f4470aeca5385f44425eee
hjjyhj <hojungkim123@gmail.com>
2022-04-03 17:43:43 -0400

    Create Squares of a Sorted Array.py

---
+++ Blind 75 LeetCode/Squares of a Sorted Array/Squares of a Sorted Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = []
+        for n in nums:
+            res.append(n**2)
+        res.sort()
+        return res
