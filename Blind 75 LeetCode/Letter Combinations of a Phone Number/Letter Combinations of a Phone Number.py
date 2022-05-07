7ae40ab64069832213be7baac77217b24f026168
hjjyhj <hojungkim123@gmail.com>
2022-05-07 17:25:10 -0400

    Create Letter Combinations of a Phone Number.py
    backtracking

---
+++ Blind 75 LeetCode/Letter Combinations of a Phone Number/Letter Combinations of a Phone Number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        dToChar = { "2": "abc",
+                    "3": "def",
+                    "4": "ghi",
+                    "5": "jkl",
+                    "6": "mno",
+                    "7": "pqrs",
+                    "8": "tuv",
+                    "9": "wxyz"}
+        
+        def backtracking(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            
+            for c in dToChar[digits[i]]:
+                backtracking(i+1, curStr+c)
+        
+        if digits:
+            backtracking(0, "")
+        return res
